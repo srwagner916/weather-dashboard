3c7db224c532a11bc803127e0727ac0405e09907
Shay Wagner <srwagner916@gmail.com>
2021-04-22 17:37:27 -0500

    can get weather data with a nested api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,33 @@
 var apiKey = "89893227e2cfb5edad9b90f12127f1ef"
 
-// var getCoordinates = function(){
-//   var city = $('#search-city').val();
-//   // fetch call to get coordinates
-//   fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`)
-//     .then(function(response){
-//       return response.json;
-//     })
-//     .then(function(data){
-//       console.log(data);
-//     });
-// };
-
-//submit event listener
-
-$('#search-city-form').submit(function(event){
-    var city = $('#search-city').val();
+var getWeather = function(){
+  var city = $('#search-city').val();
   // fetch call to get coordinates
   fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`)
-    .then(function(response){
-       return response.json();
+    .then(function(coordResponse){
+      return coordResponse.json();
     })
-    .then(function(data){
-      console.log(data);
-      var coordinates = {lat: data[0].lat, long: data[0].lon};
-      return coordinates
+    .then(function(coordData){
+      console.log(coordData);
+      // assign lat and lon values to variables
+      var lat = coordData[0].lat;
+      var lon = coordData[0].lon;
+
+      // second fetch call
+      return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+        .then(function(weatherResponse){
+          return weatherResponse.json();
+        })
+        .then(function(weatherData){
+          console.log(weatherData);
+        });
     });
+};
+
+//submit event listener
+$('#search-city-form').submit(function(event){
+  // fetch call to get coordinates
+  getWeather();
   event.preventDefault();
 });
 
